4018a3724a81195a84be20ce20c18dc6bc58bfcb
Jan K. S <wallysalami@gmail.com>
2021-07-20 19:39:30 -0300

    Add another circuit example

---
+++ docs/examples/analog_control/analog_control.py
@@ -0,0 +1,34 @@
+# This example keeps the setup code in a different file.
+# It is useful if you want to hide that complexity from students.
+
+from setup import run
+
+@run
+def programa():
+    
+    from gpiozero import PWMLED, Motor, Servo, MCP3008, Button
+    from time import sleep
+
+
+    led = PWMLED(21)
+    motor = Motor(22, 23)
+    servo = Servo(24)
+
+    potenciometer1 = MCP3008(0)
+    potenciometer2 = MCP3008(2)
+    potenciometer3 = MCP3008(6)
+    switch = Button(15)
+
+
+    while True:
+        led.value = potenciometer1.value
+
+        if switch.is_pressed:
+            motor.forward(potenciometer2.value)
+        else:
+            motor.backward(potenciometer2.value)
+          
+        servo.value = 1 - 2 * potenciometer3.value
+
+
+        sleep(0.05)
---
+++ docs/examples/analog_control/setup.py
@@ -0,0 +1,47 @@
+from tkgpio import TkCircuit
+
+configuration = {
+    "name": "Analog Control",
+    "width": 700,
+    "height": 350,
+    
+    "leds": [
+        {"x": 105, "y": 80, "name": "LED", "pin": 21}
+    ],
+    "motors": [
+        {"x": 316, "y": 80, "name": "DC Motor", "forward_pin": 22, "backward_pin": 23}
+    ],
+    "servos": [
+        {"x": 537, "y": 80, "name": "Servomotor", "pin": 24, "min_angle": -90, "max_angle": 90, "initial_angle": 20}
+    ],
+    
+    "adc": {
+      "mcp_chip": 3008,
+      "potenciometers": [
+        {"x": 40,  "y": 200, "name": "Brightness Potentiometer", "channel": 0},
+        {"x": 270, "y": 200, "name": "Speed Potentiometer",      "channel": 2},
+        {"x": 500, "y": 200, "name": "Angle Potentiometer",      "channel": 6}
+      ]  
+    },
+    "toggles": [
+        {"x": 270, "y": 270, "name": "Direction Toggle Switch", "pin": 15, "off_label": "backward", "on_label": "forward", "is_on": False}
+    ],
+    
+    # Labels have the same properties as the ones in TkInter.
+    # That means you can use them to write formatted text or draw rectangles below the circuit.
+    "labels": [
+        {"x": 15,  "y": 35, "width": 25, "height": 18, "borderwidth": 2, "relief": "solid"},
+        {"x": 56,  "y": 26, "text": "Brightness Control"},
+        
+        {"x": 245, "y": 35, "width": 25, "height": 18, "borderwidth": 2, "relief": "solid"},
+        {"x": 298, "y": 26, "text": "Speed Control"},
+        
+        {"x": 475, "y": 35, "width": 25, "height": 18, "borderwidth": 2, "relief": "solid"},
+        {"x": 531, "y": 26, "text": "Angle Control"}
+    ]
+}
+
+def run (main_function):
+    circuit = TkCircuit(configuration)
+    circuit.run(main_function)
+    
