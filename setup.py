affd6c788e3ad8bd6d1ba0716139a037a01b163d
Jan K. S <wallysalami@gmail.com>
2021-07-21 20:26:08 -0300

    Update setup files

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="tkgpio",
-    version="0.1",
-    author="Jan K. S.",
-    author_email="developer@janks.software",
-    description="A Python library to simulate Raspberry Pi GPIO devices in TkInter",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/wallysalami/tkgpio",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "gpiozero",
-        "numpy",
-        "Pillow",
-        "scipy",
-        "sounddevice",
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.5',
-    include_package_data=True,
-    package_data={
-        "tkgpio": ["resources/**/*"],
-    }
-)
+if __name__ == "__main__":
+    setup()
